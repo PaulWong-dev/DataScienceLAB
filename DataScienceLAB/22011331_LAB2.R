42d4bd4343dad1f26804ef634ab1dd97495c5214
PaulWong <paulwongkh1234@outlook.com>
2026-02-09 00:18:40 +0800

    Add files via upload

---
+++ DataScienceLAB/22011331_LAB2.R
@@ -0,0 +1,9 @@
+weight <- 85
+height <- 1.75
+bmi <- weight / (height ^ 2)
+cat("Calculated BMI:", round(bmi, 1), "\n\n")
+
+cat("Underweight:", bmi <= 18.4, "\n")
+cat("Normal:", bmi >= 18.5 & bmi <= 24.9, "\n")
+cat("Overweight:", bmi >= 25.0 & bmi <= 39.9, "\n")
+cat("Obese:", bmi >= 40.0, "\n")
