30476b3ed7f339df247a3b6372b8ca9277308e98
PaulWong <paulwongkh1234@outlook.com>
2026-02-09 10:22:41 +0800

    Delete DataScienceLAB directory

--- DataScienceLAB/Lab2/22011331_LAB2Q2.R
+++
@@ -1,6 +0,0 @@
-string1 <- readline(prompt = "Enter string 1: ")
-string2 <- readline(prompt = "Enter string 2: ")
-
-result <- tolower(string1) == tolower(string2)
-
-cat("This program compare 2 strings. Both inputs are similar:", result, "\n")
